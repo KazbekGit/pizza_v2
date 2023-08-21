33dbad3ddfbd094b4e585371adcada556d6c18ae
Kazbekgit <kazbekgit@gmail.com>
2023-08-21 21:10:11 +0300

    filtering start

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+
 import Header from "./components/UI/Header/Header";
-import Categories from "./components/UI/Categories/Categories";
-import Sort from "./components/UI/Sort/Sort";
-import PizzaBlock from "./components/UI/PizzaBlock.jsx/PizzaBlock";
-import Skeleton from "./components/UI/Skeleton/Skeleton";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [pizzas, setPizzas] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchPizzas = () => {
-    try {
-      fetch("https://64db6924593f57e435b0ec78.mockapi.io/pizzas")
-        .then((res) => res.json())
-        .then((json) => setPizzas(json));
-    } catch (err) {
-      console.warn(err);
-    } finally {
-      setIsLoading(false);
-    }
+  const NotFoundBlock = () => {
+    return (
+      <div>
+        <h2>404ERROR</h2>
+        <Link to="/"><button className="back">BaCK</button></Link>
+      </div>
+    );
   };
-
-  useEffect(() => {
-    fetchPizzas();
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(pizzas.length)].map((_, index) => {
-                  return <Skeleton key={index}/>;
-                })
-              : pizzas.map((pizza) => {
-                  return <PizzaBlock {...pizza} key={pizza.id} />;
-                })}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFoundBlock />} />
+          </Routes>
         </div>
       </div>
     </div>
